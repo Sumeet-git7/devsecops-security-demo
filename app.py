01214e55cc3f6feb9dce1fc0b973fa526971f005
Sumeet-git7 <22070692@ycce.in>
2026-03-07 12:47:02 +0530

    testing 3

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-import os
+"""import os
 
 def run():
     user_input = input("Enter command: ")
     os.system(user_input)
+
+if __name__ == "__main__":
+    run()
+
+"""
+
+
+import subprocess
+
+def run():
+    user_input = input("Enter text: ")
+    subprocess.run(["echo", user_input], shell=False)
 
 if __name__ == "__main__":
     run()
